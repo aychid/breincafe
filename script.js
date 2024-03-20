768222d81058c075118830575c4ad6e805396506
aychid <aydinnajlhossaini@hotmail.com>
2024-03-20 14:20:40 -0400

    feat: add language button with js and favicon

---
+++ script.js
@@ -0,0 +1,15 @@
+// Select the button and greeting element
+const languageButton = document.getElementById('language-button');
+
+// Add click event listener to the button
+languageButton.addEventListener('click', () => {
+  // Toggle between English and Papiamentu
+  if (languageButton.getAttribute('data-lang') === 'en') {
+    languageButton.setAttribute('data-lang', 'pa');
+    languageButton.textContent = 'Papiamentu';
+  } else {
+    languageButton.setAttribute('data-lang', 'en');
+    languageButton.textContent = 'English';
+  }
+});
+// TODO add to github repo
